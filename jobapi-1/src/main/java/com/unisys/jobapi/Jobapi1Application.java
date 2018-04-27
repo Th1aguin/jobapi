bfcb3e8464ca11d1456d54be233a3cd580b6a436
James Bond <bond@unknown.com>
2018-04-27 19:34:44 -0300

    first commit

---
+++ jobapi-1/src/main/java/com/unisys/jobapi/Jobapi1Application.java
@@ -0,0 +1,12 @@
+package com.unisys.jobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobapi1Application.class, args);
+	}
+}
---
+++ jobapi-1/src/main/java/com/unisys/jobapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.unisys.jobapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jobapi1Application.class);
+	}
+
+}
